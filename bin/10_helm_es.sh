360f087f2da8b9806fc17cbeee74b288e9614fb5
Lukas Beranek <lukas@momence.com>
2023-06-01 22:36:08 +0200

    add helm chart and some bin files

--- bin/10_helm_es.sh
+++ bin/10_helm_es.sh
@@ -3,5 +3,10 @@
 kubectl config set-context docker-desktop
 # Install external secrets operator using helm
 helm repo update &&
-helm install external-secrets external-secrets/external-secrets --namespace external-secrets --create-namespace
+helm upgrade --install external-secrets external-secrets/external-secrets --namespace external-secrets --create-namespace
 
+helm upgrade --install sealed-secrets sealed-secrets/sealed-secrets -n sealed-secrets --create-namespace
+
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace ingress-nginx --create-namespace
--- bin/20_helm_echo.sh
+++ bin/20_helm_echo.sh
@@ -7,6 +7,6 @@ helm repo update &&
 helm upgrade --install echo-server ./charts/echo-server --namespace echo-server --create-namespace \
     --set application.enable.file=false \
     --set application.enable.header=false \
-    --set application.enble.request=false \
+    --set application.enable.request=false \
     --set application.enable.http=false \
     --set application.enable.host=false
