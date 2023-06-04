7ba23f2498d2431ebf8baa1e858fc34cf60538c4
Lukas Beranek <lukas@momence.com>
2023-06-04 21:37:02 +0200

    cleanup

--- bin/40_helm_es_store.sh
+++ bin/40_helm_es_store.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 # Install external secrets operator using helm
-kubectl apply -f ./bin/manifests/30_secret_store_v1.yaml
+kubectl apply -f ./bin/manifests/30_secret_store.yaml
 
