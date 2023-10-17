b65b416999e026f428f20f721ba58f3f6d51dfbe
Lukas Beranek <lukas@momence.com>
2023-10-17 22:09:09 +0200

    add comments

--- bin/30_helm_es.sh
+++ bin/30_helm_es.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 # Install external secrets operator using helm
-helm upgrade --install external-secrets external-secrets/external-secrets --namespace external-secrets --create-namespace
+helm upgrade --install external-secrets external-secrets/external-secrets \
+--namespace external-secrets --create-namespace
