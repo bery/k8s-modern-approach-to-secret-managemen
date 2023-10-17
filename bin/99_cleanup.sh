4b72289c5cf6ffb8a7df4ff8d6a53e19864b4a79
Lukas Beranek <lukas@momence.com>
2023-10-17 22:12:36 +0200

    add comments

--- bin/99_cleanup.sh
+++ bin/99_cleanup.sh
@@ -4,4 +4,4 @@ set -e
 # Install external secrets operator using helm
 
 helm del echo-server --namespace echo-server
-helm del external-secrets --namespace external-secrets
+# helm del external-secrets --namespace external-secrets
